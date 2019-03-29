788c31b534b543f11023dee810bc3916e3ae9d1b
Jesper <jesp626a@edu.eal.dk>
2019-03-29 23:39:35 +0100

    Refactored EntityGhost and added TileTypes check

--- Assets/Scripts/EntityGhost.cs
+++ Assets/Scripts/EntityGhost.cs
@@ -1,78 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class EntityGhost : MonoBehaviour {
-    private World _world;
-    private int _sizeX;
-    private int _sizeY;
+public class EntityGhost : MonoBehaviour
+{
+	private World _world;
+	private int _sizeX;
+	private int _sizeY;
 
-    private GameObject[] _ghosts;
-    private WorldTile[] _tiles;
-    private Camera _camera;
+	private GameObject[] _ghosts;
+	private WorldTile[] _tiles;
+	private Camera _camera;
 
-    private void Start() {
-        _camera = Camera.main;
-        _world = FindObjectOfType<World>();
+	private bool _canPlace;
 
-        SetGhostSize(2, 2);
-    }
+	private void Start()
+	{
+		_camera = Camera.main;
+		_world = FindObjectOfType<World>();
 
-    private void Update() {
-        CheckArea();
+		SetGhostSize(2, 1);
+	}
 
-        if (Input.GetKeyDown(KeyCode.E)) {
-            TryPlaceBuilding();
-        }
-    }
+	private void Update()
+	{
+		CheckArea(new TileType[] { TileType.Grass, TileType.MarsDirt });
 
-    private void SetGhostSize(int x, int y) {
-        _sizeX = x;
-        _sizeY = y;
+		if (Input.GetKeyDown(KeyCode.E) && _canPlace)
+		{
+			PlaceEntity();
+		}
+	}
 
-        var size = _sizeX * _sizeY;
+	private void SetGhostSize(int x, int y)
+	{
+		_sizeX = x;
+		_sizeY = y;
 
-        _ghosts = new GameObject[size];
-        _tiles = new WorldTile[size];
+		var size = _sizeX * _sizeY;
+
+		_ghosts = new GameObject[size];
+		_tiles = new WorldTile[size];
 
 
-        for (var i = 0; i < size; i++) {
-            var o = new GameObject();
-            o.transform.parent = transform;
-            o.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/DefaultTile");
-            o.GetComponent<SpriteRenderer>().sortingOrder = 1;
-            _ghosts[i] = o;
-        }
-    }
+		for (var i = 0; i < size; i++)
+		{
+			var o = new GameObject();
+			o.transform.parent = transform;
+			o.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/DefaultTile");
+			o.GetComponent<SpriteRenderer>().sortingOrder = 1;
+			_ghosts[i] = o;
+		}
+	}
 
-    private void CheckArea() {
-        Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
-        var clamp = new Vector2(Mathf.RoundToInt(mousePosition.x), Mathf.RoundToInt(mousePosition.y));
+	private void CheckArea(TileType[] allowedTileTypes)
+	{
+		Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+		var clamp = new Vector2(Mathf.RoundToInt(mousePosition.x), Mathf.RoundToInt(mousePosition.y));
 
-        var i = 0;
-        for (var y = 0; y < _sizeY; y++) {
-            for (var x = 0; x < _sizeX; x++) {
-                var tile = _world.GetTileFromWorldPosition(clamp.x + x, clamp.y + y);
-                _ghosts[i].transform.position = new Vector2((int) clamp.x + x, (int) clamp.y + y);
-                _ghosts[i].GetComponent<SpriteRenderer>().color = tile.Occupied == false ? Color.green : Color.red;
-                i++;
-            }
-        }
-    }
+		var i = 0;
+		for (var y = 0; y < _sizeY; y++)
+		{
+			for (var x = 0; x < _sizeX; x++)
+			{
+				var tile = _world.GetTileFromWorldPosition(clamp.x + x, clamp.y + y);
+				_ghosts[i].transform.position = new Vector2((int)clamp.x + x, (int)clamp.y + y);
 
-    private void TryPlaceBuilding() {
-        foreach (WorldTile tile in _tiles) {
-            if (tile.Occupied) {
-                Debug.Log("Cant Place Building");
-                return;
-            }
-        }
+				if (tile.Occupied == false && allowedTileTypes.Contains(tile.TileType))
+				{
+					_ghosts[i].GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 0.5f);
+					_canPlace = true;
+				}
+				else
+				{
+					_ghosts[i].GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 0.5f);
+					_canPlace = false;
+				}
+				_tiles[i] = tile;
+				i++;
+			}
+		}
+	}
 
-        foreach (WorldTile tile in _tiles) {
-            tile.Occupied = true;
+	private void PlaceEntity()
+	{
+		foreach (WorldTile tile in _tiles)
+		{
+			tile.Occupied = true;
 
-            //TODO: Place actual entity
-            tile.Sprite = Resources.Load<Sprite>("Sprites/DebugGrey");
-        }
-    }
+			//TODO: Place actual entity
+			tile.Sprite = Resources.Load<Sprite>("Sprites/DebugGrey");
+		}
+	}
 }
--- Assets/Scripts/WorldTile.cs
+++ Assets/Scripts/WorldTile.cs
@@ -7,6 +7,7 @@ public class WorldTile : MonoBehaviour
 	private Sprite _sprite;
 
 	public Vector2 Position;
+	public TileType TileType;
 
 	public bool Occupied;
 
@@ -22,6 +23,31 @@ public class WorldTile : MonoBehaviour
 
 	private void Start()
 	{
-		Sprite = Resources.Load<Sprite>("Sprites/DebugSquare");
+		//Sprite = Resources.Load<Sprite>("Sprites/DebugMarsDirt");
+		DebugSetRandomTileType();
+	}
+
+	//TODO: Remove this
+	//------ DEBUG -------
+	private void DebugSetRandomTileType()
+	{
+
+		if(Random.Range(0, 100) < 50)
+		{
+			Sprite = Resources.Load<Sprite>("Sprites/DebugMarsDirt");
+			TileType = TileType.MarsDirt;
+		}
+		else
+		{
+			Sprite = Resources.Load<Sprite>("Sprites/DebugGrass");
+			TileType = TileType.Grass;
+		}
 	}
 }
+
+public enum TileType
+{
+	MarsDirt = 0,
+	Grass
+}
+
