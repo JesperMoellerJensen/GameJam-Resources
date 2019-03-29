b64dee05bce696c5000292f62fa5976f5b41fc4f
kristoffer petersen <krizzmp@gmail.com>
2019-03-29 22:41:42 +0100

    refactor

--- Assets/Scripts/EntityGhost.cs
+++ Assets/Scripts/EntityGhost.cs
@@ -2,61 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EntityGhost : MonoBehaviour
-{
-	private World _world;
-	private int _sizeX;
-	private int _sizeY;
+public class EntityGhost : MonoBehaviour {
+    private World _world;
+    private int _sizeX;
+    private int _sizeY;
 
-	private GameObject[] _ghosts;
-	private WorldTile[] _tiles;
-	private Camera _camera;
+    private GameObject[] _ghosts;
+    private WorldTile[] _tiles;
+    private Camera _camera;
 
-	private void Start()
-	{
-		_camera = Camera.main;
-		_world = FindObjectOfType<World>();
+    private void Start() {
+        _camera = Camera.main;
+        _world = FindObjectOfType<World>();
 
-		SetGhostSize(2, 2);
-	}
+        SetGhostSize(2, 2);
+    }
 
-	private void Update()
-	{
-		CheckArea();
+    private void Update() {
+        CheckArea();
 
-		if (Input.GetKeyDown(KeyCode.E))
-		{
-			TryPlaceBuilding();
-		}
-	}
+        if (Input.GetKeyDown(KeyCode.E)) {
+            TryPlaceBuilding();
+        }
+    }
 
-	private void SetGhostSize(int x, int y)
-	{
-		_sizeX = x;
-		_sizeY = y;
+    private void SetGhostSize(int x, int y) {
+        _sizeX = x;
+        _sizeY = y;
 
-		var size = _sizeX * _sizeY;
+        var size = _sizeX * _sizeY;
 
-		_ghosts = new GameObject[size];
-		_tiles = new WorldTile[size];
+        _ghosts = new GameObject[size];
+        _tiles = new WorldTile[size];
 
 
-		for (var i = 0; i < size; i++)
-		{
-			var o = new GameObject();
-			o.transform.parent = transform;
-			o.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/DefaultTile");
-			o.GetComponent<SpriteRenderer>().sortingOrder = 1;
-			_ghosts[i] = o;
-		}
-	}
+        for (var i = 0; i < size; i++) {
+            var o = new GameObject();
+            o.transform.parent = transform;
+            o.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/DefaultTile");
+            o.GetComponent<SpriteRenderer>().sortingOrder = 1;
+            _ghosts[i] = o;
+        }
+    }
 
-	private void CheckArea()
-	{
-		Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
-		var clamp = new Vector2(Mathf.RoundToInt(mousePosition.x), Mathf.RoundToInt(mousePosition.y));
+    private void CheckArea() {
+        Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        var clamp = new Vector2(Mathf.RoundToInt(mousePosition.x), Mathf.RoundToInt(mousePosition.y));
 
-	var i = 0;
+        var i = 0;
         for (var y = 0; y < _sizeY; y++) {
             for (var x = 0; x < _sizeX; x++) {
                 var tile = _world.GetTileFromWorldPosition(clamp.x + x, clamp.y + y);
@@ -66,27 +59,20 @@ public class EntityGhost : MonoBehaviour
             }
         }
     }
-	}
 
-	private void TryPlaceBuilding()
-	{
-		foreach (WorldTile tile in _tiles)
-		{
-			if (tile.Occupied)
-			{
-				Debug.Log("Cant Place Building");
-				return;
-			}
-		}
+    private void TryPlaceBuilding() {
+        foreach (WorldTile tile in _tiles) {
+            if (tile.Occupied) {
+                Debug.Log("Cant Place Building");
+                return;
+            }
+        }
 
-		foreach (WorldTile tile in _tiles)
-		{
-			tile.Occupied = true;
-			
-			//TODO: Place actual entity
-			tile.Sprite = Resources.Load<Sprite>("Sprites/DebugGrey");
-		}
+        foreach (WorldTile tile in _tiles) {
+            tile.Occupied = true;
 
-	}
-        
+            //TODO: Place actual entity
+            tile.Sprite = Resources.Load<Sprite>("Sprites/DebugGrey");
+        }
+    }
 }
