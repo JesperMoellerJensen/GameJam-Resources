ae167962c7b147679216108720df0b8b1bde761f
Jesper <jesp626a@edu.eal.dk>
2019-03-29 20:23:37 +0100

    Unity Files

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+	private WorldTile[,] _tileMap;
+
+	public int WORLD_SIZE = 10;
+	public GameObject TilePrefab;
+
+	private void Start()
+	{
+		_tileMap = new WorldTile[WORLD_SIZE, WORLD_SIZE];
+		InitializeTiles();
+	}
+
+
+	private void InitializeTiles()
+	{
+		for (int y = 0; y < WORLD_SIZE; y++)
+		{
+			for (int x = 0; x < WORLD_SIZE; x++)
+			{
+				_tileMap[x, y] = Instantiate(TilePrefab, new Vector2(x, y), transform.rotation,transform).GetComponent<WorldTile>();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/WorldTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldTile : MonoBehaviour
+{
+	private Sprite _sprite;
+
+	public Vector2 Position { get; set; }
+	public Sprite Sprite
+	{
+		get { return _sprite; }
+		set
+		{
+			_sprite = value;
+			GetComponent<SpriteRenderer>().sprite = _sprite;
+		}
+	}
+
+	private void Start()
+	{
+		Sprite = Resources.Load<Sprite>("Sprites/DefaultTile");
+	}
+}
