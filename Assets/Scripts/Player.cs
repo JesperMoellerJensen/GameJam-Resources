1ee4378fcf0c323de542321770b251370a47f91d
Jesper <jesp626a@edu.eal.dk>
2019-03-30 01:36:53 +0100

    Added Player stats and oxygen tank Usage

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,21 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-	public float MaxHealth;
+[RequireComponent(typeof(PlayerOxygenTank))]
+public class Player : MonoBehaviour {
+    public float MaxHealth;
+    public float OxygenTickSpeed;
+    public float OxygenConsumptionn;
+    public float NoOxygenDamage;
 
-	private PlayerOxygenTank _oxygenTank;
-	private float _currentHealth;
+    private PlayerOxygenTank _oxygenTank;
+    private float _currentHealth;
 
-	private void Start()
-	{
-		InvokeRepeating(UseOxygen)
-	}
 
-	private void UseOxygen()
-	{
+    private void Awake() {
+        _currentHealth = MaxHealth;
+    }
+    private void Start() {
+        _oxygenTank = GetComponent<PlayerOxygenTank>();
 
-	}
+        InvokeRepeating("UseOxygen", 0, OxygenTickSpeed);
+    }
+
+    public void TakeDamage(float amount) {
+        _currentHealth -= amount;
+        Debug.Log($"I took {amount} damage, i now have {_currentHealth} health left");
+        if(_currentHealth <= 0) {
+            Death();
+        }
+    }
+
+    private void UseOxygen() {
+        if (_oxygenTank.UseOxygen(OxygenConsumptionn)) {
+            Debug.Log($"I can breathe, and i have {_oxygenTank.CurrentCapacity}");
+        } else {
+            Debug.Log("I CANT BREATHE!");
+            TakeDamage(NoOxygenDamage);
+        }
+    }
+
+    private void Death() {
+        Debug.Log("I Am Dead");
+    }
+
+    
 
 }
--- Assets/Scripts/PlayerOxygenTank.cs
+++ Assets/Scripts/PlayerOxygenTank.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerOxygenTank : MonoBehaviour
-{
-	public float MaxCapacity;
+public class PlayerOxygenTank : MonoBehaviour {
+    public float MaxCapacity;
 
-	private float _currentCapacity;
+    public float CurrentCapacity;
+    private void Awake() {
+        CurrentCapacity = MaxCapacity;
+    }
 
+    public bool UseOxygen(float amount) {
+        CurrentCapacity = Mathf.Clamp(CurrentCapacity - amount, 0, MaxCapacity);
+        return CurrentCapacity > 0;
+    }
 
-	public bool UseOxygen(float amount)
-	{
+    //public float RefillOxygen(float amount) {
 
-	}
-
-	public float RefillOxygen(float amount)
-	{
-
-	}
+    //}
 }
